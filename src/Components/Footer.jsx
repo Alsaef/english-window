1ff931128abd89d437a2b69330484aa772b68368
Alsaef <saef.ratul@gmail.com>
2026-06-30 12:37:56 +0600

    backend  updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div>
             <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4 h-20">
                 <aside>
-                    <p>Copyright © {new Date().getFullYear()} - All right reserved by <a href="https://developer-ratul.netlify.app/" className='font-bold text-black'>Al Saef Ratul</a></p>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved by <a target='blank' href="https://developer-ratul.netlify.app/" className='font-bold text-black'>Al Saef Ratul</a></p>
                 </aside>
             </footer>
         </div>
